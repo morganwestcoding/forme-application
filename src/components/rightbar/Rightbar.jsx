0b0784d561ba16a7a31159962e736f69bb0c89ff
Morgan West <mewwes@gmail.com>
2023-11-30 01:48:33 -0800

    profile

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,12 +2,12 @@ import "./rightbar.css";
 import {Users} from "../../dummyData"
 import Online from "../online/Online";
 
-export default function Rightbar() {
+export default function Rightbar({profile}) {
 
-  return (
-    <div className="rightbar">
-      <div className="rightbarWrapper">
-        <div className="birthdayContainer">
+  const HomeRightBar = () => {
+    return(
+      <>
+       <div className="birthdayContainer">
           <img className="birthdayImg" src="assets/gift.png" alt="" />
           <span className="birthdayText">
             <b>Paolo Foster</b> and <b>3 other friends</b> have a birthday.
@@ -20,6 +20,33 @@ export default function Rightbar() {
           <Online key={u.id} user={u}/>
           ))}
         </ul>
+      </>
+    )
+  };
+
+  const ProfileRightBar = () =>{
+  return (
+    <>
+    <h4 className="rightbarTitle">User information</h4>
+    <div className="rightbarInfoItem">
+      <span className="rightbarInfoKey">City</span>
+      <span className="rightbarInfoValue">New York</span>
+    </div>
+    <div className="rightbarInfoItem">
+      <span className="rightbarInfoKey">From</span>
+      <span className="rightbarInfoValue">Madrid</span>
+    </div>
+    <div className="rightbarInfoItem">
+      <span className="rightbarInfoKey">Relationship:</span>
+      <span className="rightbarInfoValue">Single</span>
+    </div>
+    </>
+  )
+}
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+       <ProfileRightBar/>
       </div>
     </div>
   )
