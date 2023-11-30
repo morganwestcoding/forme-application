c16018044e4c32694ecadde98bfa1e9305008648
Morgan West <mewwes@gmail.com>
2023-11-30 01:21:34 -0800

    setIsLiked

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,12 +1,17 @@
 import "./post.css"
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { Users } from "../../dummyData";
+import { useState } from "react";
 
 export default function Post({ post }) {
     
-    const user = Users.filter(u=>u.id===1)
+    const [like,setLike] = useState(post.like)
+    const [isLiked,setIsLiked] = useState(false)
 
-    console.log(user)
+    const likeHandler =()=>{
+        setLike(isLiked ? like-1 : like+1)
+        setIsLiked(!isLiked)
+    }
   return (
     <div className="post">
         <div className="postWrapper">
@@ -26,9 +31,9 @@ export default function Post({ post }) {
             </div>
             <div className="postBottom">
                 <div className="postBottomLeft">
-                    <img className="likeIcon" src="assets/like.png" alt=""/>
-                    <img className="likeIcon" src="assets/heart.png" alt=""/>
-                    <span className="likeCounter">{post.like} people like it</span>
+                    <img className="likeIcon" src="assets/like.png" onClick={likeHandler} alt=""/>
+                    <img className="likeIcon" src="assets/heart.png" onClick={likeHandler} alt=""/>
+                    <span className="likeCounter">{like} people like it</span>
                 </div>
                 <div className="postBottomRight"><span className="postCommentText">{post.comment} comments</span></div>
             </div>
