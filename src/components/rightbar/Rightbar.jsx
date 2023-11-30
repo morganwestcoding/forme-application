a9e27283d47eefeb01241343e11f04305f66ecec
Morgan West <mewwes@gmail.com>
2023-11-29 21:58:17 -0800

    rightbar

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,6 +3,25 @@ import "./rightbar.css";
 export default function Rightbar() {
 
   return (
-    <div className="rightbar">Rightbar</div>
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="assets/gift.png" alt="" />
+          <span className="birthdayText">
+            <b>Paolo Foster</b> and <b>3 other friends</b> have a birthday.
+          </span>
+        </div>
+        <img className="rightbarAd" src="assets/ad.png" alt=""/>
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarPofileImgContainer">
+              <img className="rightbarProfileImg" src="assets/person/3.jpeg" alt=""/>
+              <span className="rightbarOnline"></span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
