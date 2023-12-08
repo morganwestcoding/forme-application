33369dd91605994afa13166d10ba1d5558ed1aa5
Morgan West <mewwes@gmail.com>
2023-12-08 13:09:29 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 
